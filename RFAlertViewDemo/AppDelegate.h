aef55ec6bf8aba07916336bead14b59f62773093
rocky <fengjian0213@126.com>
2016-10-18 16:58:38 +0800

    Initial Commit

---
+++ RFAlertViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFAlertViewDemo
+//
+//  Created by rocky on 16/10/18.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RFAlertViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RFAlertViewDemo
+//
+//  Created by rocky on 16/10/18.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
