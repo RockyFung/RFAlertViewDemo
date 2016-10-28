49a0ee48ec54174a07456b46ca4668723132751e
rocky <fengjian0213@126.com>
2016-10-28 11:35:55 +0800

    1

--- RFAlertViewDemo/AppDelegate.m
+++ RFAlertViewDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // 这是在test分支添加的内容
     return YES;
 }
 
